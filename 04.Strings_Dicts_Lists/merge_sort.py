c3a527fabaca537a1becbddf2b38d166742246af
AhmedEl-Moharakawy <ahmedsaber.21@hotmail.com>
2025-01-12 02:57:39 +0200

    04.Strings_Dicts_Lists\merge_sort.py

---
+++ 04.Strings_Dicts_Lists/merge_sort.py
@@ -0,0 +1,39 @@
+import random
+
+def merge_sort_fn(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort_fn(left_half)
+        merge_sort_fn(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+def main():
+    arr = random.sample(range(1, 101), 10)
+    print("Original list:", arr)
+    merge_sort_fn(arr)
+    print("Sorted list:", arr)
+
+main()
